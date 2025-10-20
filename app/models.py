750d8088f06b6cb7af30d525803a6b57b4c1db90
Olga <olgacherry03@gmail.com>
2025-10-20 21:42:10 +0300

    Addressed review comments

--- app/models.py
+++ app/models.py
@@ -1,9 +1,8 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 from enum import IntEnum
 
 from sqlalchemy import (
-    ARRAY,
     TIMESTAMP,
     Column,
     Enum,
@@ -13,8 +12,6 @@ from sqlalchemy import (
     PrimaryKeyConstraint,
     String,
     Table,
-    func,
-    text,
 )
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import (
@@ -34,7 +31,9 @@ naming_convention = {
     "pk": "pk_%(table_name)s",
 }
 
-UTC_NOW = func.timezone("utc", func.now())
+
+def utc_now():
+    return datetime.now(timezone.utc)
 
 
 class UserRole(IntEnum):
@@ -70,70 +69,85 @@ dictlist_words = Table(
 
 class User(Base):
     id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=lambda: uuid.uuid4, init=False
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, init=False
     )
     username: Mapped[str] = mapped_column(nullable=False, unique=True)
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
     password: Mapped[str] = mapped_column(nullable=False)
     role: Mapped[UserRole] = mapped_column(
-        Enum(UserRole, name="role_enum", values_callable=lambda x: [e.name for e in x]),
-        default=UserRole.UnauthorizedUser,
-        init=False,
+        Enum(UserRole, name="role_enum", default=UserRole.UnauthorizedUser)
     )
-    created: Mapped[datetime] = mapped_column(
-        TIMESTAMP(timezone=True), server_default=UTC_NOW, init=False
-    )
-    full_access_deadline: Mapped[datetime] = mapped_column(
-        TIMESTAMP(timezone=True),
-        server_default=text("(now() + interval '356 days')"),
-        init=False,
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(), default=utc_now, init=False
     )
 
     dict_lists: Mapped[list["DictList"]] = relationship(
         "DictList", back_populates="user", cascade="all, delete-orphan", init=False
     )
+    words: Mapped[list["Word"]] = relationship(
+        "Word", back_populates="user", init=False
+    )
 
 
 class DictList(Base):
-    id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), ForeignKey("user.id", ondelete="CASCADE"), nullable=False
+        UUID(as_uuid=True),
+        ForeignKey("user.id", ondelete="CASCADE"),
+        nullable=False,
+        init=False,
     )
-    lang_id: Mapped[int] = mapped_column(Integer, ForeignKey("language.id"))
-    name: Mapped[str] = mapped_column(default="General", nullable=False)
+    lang_code: Mapped[str] = mapped_column(ForeignKey("language.code"), init=False)
+    name: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(), default=utc_now, init=False
+    )
+    user: Mapped["User"] = relationship("User", back_populates="dict_lists")
+    language: Mapped["Language"] = relationship("Language", back_populates="dict_lists")
     max_words_limit: Mapped[int | None] = mapped_column(default=200)
-    created: Mapped[datetime] = mapped_column(
-        TIMESTAMP(timezone=True), server_default=UTC_NOW, init=False
-    )
-
-    user: Mapped["User"] = relationship("User", back_populates="dict_lists", init=False)
     words: Mapped[list["Word"]] = relationship(
         "Word", secondary=dictlist_words, back_populates="dict_lists", init=False
     )
 
 
 class Word(Base):
-    id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
-    lang_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("language.id"), nullable=False
+    id: Mapped[int] = mapped_column(primary_key=True)
+    lang_code: Mapped[str] = mapped_column(ForeignKey("language.code"), init=False)
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("user.id", ondelete="CASCADE"),
+        nullable=False,
+        init=False,
     )
     new_word: Mapped[str] = mapped_column(nullable=False)
-    translation: Mapped[str | None] = mapped_column(default=None)
-    context: Mapped[list[str]] = mapped_column(ARRAY(String), default_factory=list)
-    note: Mapped[str | None] = mapped_column(default=None)
-    created: Mapped[datetime] = mapped_column(
-        TIMESTAMP(timezone=True), server_default=UTC_NOW, init=False
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(), default=utc_now, init=False
     )
-
+    user: Mapped["User"] = relationship("User", back_populates="words")
+    language: Mapped["Language"] = relationship("Language", back_populates="words")
+    translation: Mapped[str | None] = mapped_column(default=None)
+    note: Mapped[str | None] = mapped_column(default=None)
     dict_lists: Mapped[list["DictList"]] = relationship(
         "DictList", secondary=dictlist_words, back_populates="words", init=False
     )
+    contexts: Mapped[list["WordContext"]] = relationship(
+        "WordContext", back_populates="word", init=False
+    )
+
+
+class WordContext(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    word_id: Mapped[int] = mapped_column(
+        ForeignKey("word.id", ondelete="CASCADE"), init=False
+    )
+    context: Mapped[str] = mapped_column(nullable=False)
+
+    word: Mapped["Word"] = relationship("Word", back_populates="contexts")
 
 
 class Language(Base):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    code: Mapped[str] = mapped_column(String(5), unique=True, nullable=False)
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    code: Mapped[str] = mapped_column(String(5), primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
 
     dict_lists: Mapped[list["DictList"]] = relationship(
         "DictList", back_populates="language", init=False
