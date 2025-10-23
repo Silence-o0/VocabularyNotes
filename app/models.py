e4913b01a8340834e462bbb783c72ce5d2000a54
Olga <olgacherry03@gmail.com>
2025-10-23 21:11:55 +0300

    fixed obj repr

--- app/models.py
+++ app/models.py
@@ -1,9 +1,8 @@
 import uuid
-from datetime import datetime, timezone
+from datetime import datetime
 from enum import IntEnum
 
 from sqlalchemy import (
-    TIMESTAMP,
     Column,
     Enum,
     ForeignKey,
@@ -14,6 +13,7 @@ from sqlalchemy import (
     Table,
 )
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
@@ -23,6 +23,8 @@ from sqlalchemy.orm import (
     relationship,
 )
 
+from app.utils import utc_now
+
 naming_convention = {
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
@@ -30,10 +32,6 @@ naming_convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s",
 }
-
-
-def utc_now():
-    return datetime.now(timezone.utc)
 
 
 class UserRole(IntEnum):
@@ -73,24 +71,26 @@ class User(Base):
     )
     username: Mapped[str] = mapped_column(nullable=False, unique=True)
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
-    password: Mapped[str] = mapped_column(nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False, repr=False)
     role: Mapped[UserRole] = mapped_column(
         Enum(UserRole, name="role_enum", default=UserRole.UnauthorizedUser)
     )
-    created_at: Mapped[datetime] = mapped_column(
-        TIMESTAMP(), default=utc_now, init=False
-    )
+    created_at: Mapped[datetime] = mapped_column(default=utc_now, init=False)
 
     dict_lists: Mapped[list["DictList"]] = relationship(
-        "DictList", back_populates="user", cascade="all, delete-orphan", init=False
+        "DictList",
+        back_populates="user",
+        cascade="all, delete-orphan",
+        init=False,
+        repr=False,
     )
     words: Mapped[list["Word"]] = relationship(
-        "Word", back_populates="user", init=False
+        "Word", back_populates="user", init=False, repr=False
     )
 
 
 class DictList(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
     user_id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
         ForeignKey("user.id", ondelete="CASCADE"),
@@ -99,19 +99,23 @@ class DictList(Base):
     )
     lang_code: Mapped[str] = mapped_column(ForeignKey("language.code"), init=False)
     name: Mapped[str] = mapped_column(nullable=False)
-    created_at: Mapped[datetime] = mapped_column(
-        TIMESTAMP(), default=utc_now, init=False
+    created_at: Mapped[datetime] = mapped_column(default=utc_now, init=False)
+    user: Mapped["User"] = relationship("User", back_populates="dict_lists", repr=False)
+    language: Mapped["Language"] = relationship(
+        "Language", back_populates="dict_lists", repr=False
     )
-    user: Mapped["User"] = relationship("User", back_populates="dict_lists")
-    language: Mapped["Language"] = relationship("Language", back_populates="dict_lists")
     max_words_limit: Mapped[int | None] = mapped_column(default=200)
     words: Mapped[list["Word"]] = relationship(
-        "Word", secondary=dictlist_words, back_populates="dict_lists", init=False
+        "Word",
+        secondary=dictlist_words,
+        back_populates="dict_lists",
+        init=False,
+        repr=False,
     )
 
 
 class Word(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
     lang_code: Mapped[str] = mapped_column(ForeignKey("language.code"), init=False)
     user_id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
@@ -120,29 +124,36 @@ class Word(Base):
         init=False,
     )
     new_word: Mapped[str] = mapped_column(nullable=False)
-    created_at: Mapped[datetime] = mapped_column(
-        TIMESTAMP(), default=utc_now, init=False
+    created_at: Mapped[datetime] = mapped_column(default=utc_now, init=False)
+    user: Mapped["User"] = relationship("User", back_populates="words", repr=False)
+    language: Mapped["Language"] = relationship(
+        "Language", back_populates="words", repr=False
     )
-    user: Mapped["User"] = relationship("User", back_populates="words")
-    language: Mapped["Language"] = relationship("Language", back_populates="words")
     translation: Mapped[str | None] = mapped_column(default=None)
     note: Mapped[str | None] = mapped_column(default=None)
     dict_lists: Mapped[list["DictList"]] = relationship(
-        "DictList", secondary=dictlist_words, back_populates="words", init=False
+        "DictList",
+        secondary=dictlist_words,
+        back_populates="words",
+        init=False,
+        repr=False,
     )
     contexts: Mapped[list["WordContext"]] = relationship(
-        "WordContext", back_populates="word", init=False
+        "WordContext", back_populates="word", init=False, repr=False
     )
+    contexts_list = association_proxy("contexts", "context")
 
 
 class WordContext(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
     word_id: Mapped[int] = mapped_column(
         ForeignKey("word.id", ondelete="CASCADE"), init=False
     )
     context: Mapped[str] = mapped_column(nullable=False)
 
-    word: Mapped["Word"] = relationship("Word", back_populates="contexts")
+    word: Mapped["Word"] = relationship(
+        "Word", back_populates="contexts", init=False, repr=False
+    )
 
 
 class Language(Base):
@@ -150,8 +161,8 @@ class Language(Base):
     name: Mapped[str] = mapped_column(nullable=False)
 
     dict_lists: Mapped[list["DictList"]] = relationship(
-        "DictList", back_populates="language", init=False
+        "DictList", back_populates="language", init=False, repr=False
     )
     words: Mapped[list["Word"]] = relationship(
-        "Word", back_populates="language", init=False
+        "Word", back_populates="language", init=False, repr=False
     )
